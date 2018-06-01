f930e5448fec55ce933092b590585f526b612a99
Maarten van Gompel <proycon@anaproy.nl>
2018-06-01 11:38:38 +0200

    allow vue devtools in production

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+Vue.config.devtools = true //also allow in production for now
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
