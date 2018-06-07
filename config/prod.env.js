b90931180926ba1313ce577171f2e35bc04b3398
Maarten van Gompel <proycon@anaproy.nl>
2018-06-07 22:52:38 +0200

    added support for running at a different base url

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,6 +4,7 @@ module.exports = {
   TITLE: '"Language Tool Portal"',
   DESCRIPTION: '"This portal provides an overview of available Natural Language Processing software, use the filters above to broaden or narrow the selection."',
   REGISTRY_URL: '"/metadata.json"',
+  BASE: '"/"',
   LOGO_RIGHT: 'true',
   LOGO_LEFT: 'true',
   ORGANIZATIONS: '["Radboud"]',
