784763cd686dd8a3e467977731dc8a65e6fc8fd6
Maarten van Gompel <proycon@anaproy.nl>
2018-06-07 18:26:32 +0200

    version bump and changed debug registry url

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   TITLE: '"Language Tool Portal"',
   DESCRIPTION: '"This portal provides an overview of available Natural Language Processing software, use the filters above to broaden or narrow the selection."',
-  REGISTRY_URL: '"http://localhost:8080/metadata.json"',
+  REGISTRY_URL: '"/metadata.json"',
   LOGO_RIGHT: 'true',
   LOGO_LEFT: 'true',
   ORGANIZATIONS: '["Radboud"]',
